3489e53533b2c5597e37277e3bd21c754baabc32
aliasgherman <46041895+aliasgherman@users.noreply.github.com>
2019-05-27 13:20:27 +0300

    Updated filter options
    Now only downloads the files in the current date directory. Avoids overheads.

--- downloadAutobak.py
+++ downloadAutobak.py
@@ -6,7 +6,7 @@ import pysftp
 
 temp = datetime.now()
 DIRFILTER = "AUTOBAKDATA{:04d}{:02d}{:02d}".format(temp.year, temp.month, temp.day)
-print("Will look for directory with filter " + DIRFILTER)
+#print("Will look for directory with filter " + DIRFILTER)
 
 
 class XMLDownloader:
@@ -14,13 +14,20 @@ class XMLDownloader:
     GEXPORT = 2
     NEEXPORT = 3
 
-    def __init__(self, logger):
+    def __init__(self, logger,  HOST_LIST, FOL_LIST, LOCALFOLDER, type=AUTOBAK, PATHFILTER = ""):
         self.logger = logger
         self.logger.warn(
             "Please note that this utility does not use hostkeys to verify the hosts. If this is insecure for "
             "your setup, then kindly update the code or submit a feature request.")
         self.cnopts = pysftp.CnOpts()
         self.cnopts.hostkeys = None
+        self.PATHFILTER = PATHFILTER
+        self.HOST_LIST = HOST_LIST
+        self.FOL_LIST = FOL_LIST
+        self.LOCALFOLDER = LOCALFOLDER
+        self.type = type		
+        if self.PATHFILTER.strip() == "":
+            self.PATHFILTER = DIRFILTER
 
     def get_r_portable(self, sftp, remotedir, localdir, preserve_mtime=False):
         for entry in sftp.listdir(remotedir):
@@ -34,27 +41,30 @@ class XMLDownloader:
                     pass
                 self.get_r_portable(sftp, remotepath, localpath, preserve_mtime)
             elif S_ISREG(mode):
-                self.logger.info("Current remotepath is : " + str(remotepath))
-                sftp.get(remotepath, localpath, preserve_mtime=preserve_mtime)
+                if self.PATHFILTER.strip() != "":
+                    if str(remotepath).lower().find(self.PATHFILTER.lower()) > -1:
+                        sftp.get(remotepath, localpath, preserve_mtime=preserve_mtime)
+                        self.logger.info("Current remotepath is : " + str(remotepath) + ", Filter is " + self.PATHFILTER.strip().lower())
+                
 
-    def run(self, HOST_LIST, FOL_LIST, LOCALFOLDER, type=AUTOBAK):
-        if type == self.AUTOBAK:
+    def run(self,):
+        if self.type == self.AUTOBAK:
             REMOTEFOLDER = "/ftproot/"
         else:
             self.logger.info("Other types not implemented yet.")
             return
 
-        for host in HOST_LIST:
-            self.logger.info("Attempting connection to ", host[0])
+        for host in self.HOST_LIST:
+            self.logger.info("Attempting connection to " + host[0])
             with pysftp.Connection(host[0], username=host[1], password=host[2], cnopts=self.cnopts) as sftp:
                 try:
-                    for dirs in FOL_LIST:
+                    for dirs in self.FOL_LIST:
                         self.logger.info("Attempting download from " + dirs + "on " + host[0])
                         currdir = REMOTEFOLDER + dirs + "/Data/"
                         self.logger.info("Joining via os = " + os.path.join(REMOTEFOLDER, dirs,
                                                                             "/Data/") + " , currdir = " + currdir)
                         try:
-                            self.get_r_portable(sftp, currdir, LOCALFOLDER, True)
+                            self.get_r_portable(sftp, currdir, self.LOCALFOLDER, True)
                         except Exception as e:
                             self.logger.error("Exception in dir exploration" + str(e))
 
