e87383ecf2a1853e4dd626255763f22b0704b755
aliasgherman <aliasgherman@gmail.com>
2019-06-29 12:23:34 +0300

    Some minor changes to reduce the configurable parameters in the program for easier understanding and usage.

--- app/modules/mongotoexcel/mongotoexcel.py
+++ app/modules/mongotoexcel/mongotoexcel.py
@@ -2,20 +2,23 @@ import os
 
 import pandas as pd
 from pymongo import MongoClient
-
+import logging
+from logging.handlers import RotatingFileHandler
 
 class MongoToExcel:
-    def __init__(self, logger, DBNAME, EXPORT_PATH, TABLES_NEEDED, DATE_COLUMN, EXPORT_ALL_DATES, COLUMNS_TO_DROP,
-                 TABLE_FOR_MAXDATE="NE", MONGO_USER="", MONGO_PWD="", MONGO_PORT="localhost:27017"):
+    def __init__(self, logger, DBNAME, EXPORT_PATH, TABLES_NEEDED=[], DATE_COLUMN="AAMDATE",
+                 COLUMNS_TO_DROP=['id'], DATEVALUE="",
+                 MONGO_USER="", MONGO_PWD="", MONGO_PORT="localhost:27017"):
         self.C_DBNAME = DBNAME
         self.C_DATEPARAM = DATE_COLUMN  # the column which contains date of the table xml
         self.C_COLSTODROP = COLUMNS_TO_DROP
-        self.C_MAXDATE = ""
         self.EXPORT_PATH = EXPORT_PATH
         self.TABLES_NEEDED = TABLES_NEEDED
-        self.TABLE_FOR_MAXDATE = TABLE_FOR_MAXDATE
-        self.logger = logger
-        self.EXPORT_ALL_DATES = EXPORT_ALL_DATES
+        if logger is None:
+            self.logger =self.setupLogger()
+        else:
+            self.logger = logger
+        self.DATEVALUE = DATEVALUE
         self.UNKNOWN_MAX_DATE = -1
         # mongodb://[username:password@]host1[:port1]
         if MONGO_USER.strip() != "" and MONGO_PWD.strip() != "":
@@ -25,34 +28,51 @@ class MongoToExcel:
         if len(self.TABLES_NEEDED) == 0:
             self.TABLES_NEEDED = self.get_all_colls()
 
-    def get_max_date_from_tz(self):
-        try:
-            client = MongoClient(self.connStr)
-            with client:
-                db = client[self.C_DBNAME]
-                coll = db[self.TABLE_FOR_MAXDATE]
-                MAXDATE = coll.find_one(sort=[(self.C_DATEPARAM, -1)])[self.C_DATEPARAM]
-                return (MAXDATE)
-        except:
-            self.logger.error(
-                "Exception occurred during getting max date " + str(self.C_DBNAME) + str(self.TABLE_FOR_MAXDATE) +
-                str(self.C_DATEPARAM))
-            return self.UNKNOWN_MAX_DATE
+    def setupLogger(self):
+        '''
+        Just sets up the logger and returns the logger instance which was setup with the init function.
 
-    def get_coll_as_table(self, COLLNAME, DATEVAL):
+        :return: logger instance
+        '''
+        LOG_TAG = 'Mongo2Excel'
+        self.myLogger = logging.getLogger(LOG_TAG )
+        self.myLogger.setLevel(logging.DEBUG)
+        self.fh = RotatingFileHandler(LOG_TAG  + ".log", 'a', maxBytes=20*1024*1024,
+                                      backupCount=20)
+        self.fh.setLevel(logging.INFO)
+        self.ch = logging.StreamHandler()
+        self.ch.setLevel(logging.INFO)
+
+        #self.sh = logging.StreamHandler()  #This is a stringIO based logger.
+        #self.sh.setLevel(logging.INFO)
+        #self.sh.setStream(self.loggerString)
+
+        formatter = logging.Formatter('[ %(asctime)s ] [ %(name)s ][ %(levelname)s ] %(message)s')
+        self.ch.setFormatter(formatter)
+        #self.sh.setFormatter(formatter)
+        self.fh.setFormatter(formatter)
+
+        self.myLogger.addHandler(self.ch)
+        self.myLogger.addHandler(self.fh)
+        #self.myLogger.addHandler(self.sh)
+        return self.myLogger
+
+
+    def get_coll_as_table(self, COLLNAME):
         try:
             client = MongoClient(self.connStr)
             with client:
                 db = client[self.C_DBNAME]
                 coll = db[COLLNAME]
-                if self.EXPORT_ALL_DATES == False:
-                    return (pd.DataFrame(list(coll.find({self.C_DATEPARAM: DATEVAL}))).drop(self.C_COLSTODROP, axis=1))
+                MAXDATE = coll.find_one(sort=[(self.C_DATEPARAM, -1)])[self.C_DATEPARAM]
+                if self.DATEVALUE == "":
+                    return (pd.DataFrame(list(coll.find({self.C_DATEPARAM: MAXDATE}))).drop(self.C_COLSTODROP, axis=1))
                 else:
-                    return (pd.DataFrame(list(coll.find())).drop(self.C_COLSTODROP, axis=1))
+                    return (pd.DataFrame(list(coll.find({self.C_DATEPARAM: self.DATEVALUE}))).drop(self.C_COLSTODROP, axis=1))
         except:
             self.logger.info(
                 "Exception occurred during getting collection data. " + str(self.C_DBNAME) + ", " + str(COLLNAME) +
-                ", " + str(self.C_DATEPARAM) + ", " + str(DATEVAL))
+                ", " + str(self.C_DATEPARAM) + ", MAXDATE/DATEVAL = " + str(MAXDATE) + "/" + str(self.DATEVALUE))
             return pd.DataFrame()  # return an empty dataframe
 
     def get_all_colls(self):
@@ -60,26 +80,26 @@ class MongoToExcel:
             client = MongoClient(self.connStr)
             with client:
                 db = client[self.C_DBNAME]
-                coll = db.collection_names()
+                coll = db.list_collection_names()
+                self.logger.info("Got all collection names. Total = " + str(len(coll)))
                 return coll
-        except:
+        except Exception as e:
             self.logger.info(
-                "Exception occurred during getting all collection names. " + str(self.C_DBNAME))
+                "Exception occurred during getting all collection names. " + str(self.C_DBNAME) + ". " + str(e))
             return []
 
     def run(self):
         try:
-            MAXDATETZ = self.get_max_date_from_tz()
             export_FILENAME = os.path.join(self.EXPORT_PATH, self.C_DBNAME + ".xlsx")
             if os.path.exists(self.EXPORT_PATH) == False:
                 os.makedirs(self.EXPORT_PATH)
-
             sCount = 0
+            if len(self.TABLES_NEEDED) == 0:
+                self.TABLES_NEEDED = self.get_all_colls()
             with pd.ExcelWriter(export_FILENAME, engine='xlsxwriter',
                                 options={'strings_to_numbers': True}) as writer:
                 for tables in self.TABLES_NEEDED:
-
-                    df = self.get_coll_as_table(tables, MAXDATETZ)
+                    df = self.get_coll_as_table(tables)
                     if len(df) > 0:
                         df.to_excel(writer, sheet_name=str(sCount) + "_" + tables[:25], index=False)
                         sCount = sCount + 1
@@ -89,3 +109,9 @@ class MongoToExcel:
             self.logger.info("All tables processed. " + str(sCount))
         except Exception as e:
             self.logger.error("An error occurred while trying to export tables from Mongo DB. " + str(e))
+
+
+#exporter = MongoToExcel(logger=None, DBNAME="1-BTS3900", EXPORT_PATH="/home/aamhabiby/Desktop/resources/",
+#                        TABLES_NEEDED=[], DATE_COLUMN="AAMDATE", DATEVALUE="20190522",
+#                        COLUMNS_TO_DROP=['_id'])
+#exporter.run()
